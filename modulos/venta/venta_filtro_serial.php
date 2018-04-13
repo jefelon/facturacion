f665f11c17255727007a3b585f61be833ffdd34d
jefelon <ronilson_2006_2@hotmail.com>
2018-04-12 19:47:50 -0500

    avance vista serial
    avance para la vista de busqueda de series...

--- modulos/venta/venta_filtro_serial.php
+++ modulos/venta/venta_filtro_serial.php
@@ -8,8 +8,16 @@ session_start();
 		icons: {primary: "ui-icon-search"},
 	});
 	$('#btn_resfil').button({
-		icons: {primary: "ui-icon-arrowrefresh-1-w"},
-	});
+        icons: {primary: "ui-icon-arrowrefresh-1-w"},
+
+    });
+    $('#btn_filtrardni').button({
+        icons: {primary: "ui-icon-search"},
+    });
+    $('#btn_resfildni').button({
+        icons: {primary: "ui-icon-arrowrefresh-1-w"},
+
+    });
 
 function test(){
 	venta_tabla();
@@ -101,29 +109,40 @@ $(function() {
 <input name="hdd_modo" id="hdd_modo" type="hidden" value="venta_tabla_serial.php">
 
     <input name="hdd_action" id="hdd_action" type="hidden" value="filtro_doc">
-<fieldset><legend>Buscar por Documentos</legend>
 
-<center>
+<div style="width: 80%;margin: 0 auto">
 
-    <label for="cmb_fil_ven_doc" align="left">Tipo de Documento:</label>
-    <select name="cmb_fil_ven_doc" id="cmb_fil_ven_doc"></select>
+    <fieldset style="width: 40%;float: left"><legend>Buscar por N° de Comprobante</legend>
+        <label for="cmb_fil_ven_doc" align="left">Tipo de Documento:</label>
+        <select name="cmb_fil_ven_doc" id="cmb_fil_ven_doc"></select>
 
-    <br/>
-        
-    <label for="txt_fil_numdoc" align="left">Número de Documento:</label>
-    <input type="text" id="txt_fil_numdoc" name="txt_fil_numdoc" size="20" placeholder="Ingrese Nro. de Documento" value="<?php echo $_POST['numdoc']?>" />
+        <br/>
 
-    <br/>
+        <label for="txt_fil_numdoc" align="left">Número de Comprobante:</label>
+        <input type="text" id="txt_fil_numdoc" name="txt_fil_numdoc" size="20" placeholder="Ejemplo: F001-00566" value="<?php echo $_POST['numdoc']?>" />
 
-    <label for="txt_fil_serial" align="left">Serial:</label>
-    <input type="text" id="txt_fil_serial" name="txt_fil_serial" size="20" placeholder="Ingrese Serial" maxlength="8" value="<?php echo $_POST['serial']?>" />
+        <br/>
+    <!--
+        <label for="txt_fil_serial" align="left">Serial:</label>
+        <input type="text" id="txt_fil_serial" name="txt_fil_serial" size="20" placeholder="Ingrese Serial" maxlength="8" value="<?php echo $_POST['serial']?>" />
+    -->
+        <br/>
 
-    <br/>
+        <a href="#" onClick="test()" id="btn_filtrar">Buscar Documento</a>
+        <a href="#" onClick="venta_filtro()" id="btn_resfil">Restablecer</a>
+    </fieldset>
 
-  	<a href="#" onClick="test()" id="btn_filtrar">Buscar Documento</a>
-    <a href="#" onClick="venta_filtro()" id="btn_resfil">Restablecer</a>
+    <fieldset style="width: 40%;float: right"><legend>Buscar por DNI/RUC</legend>
+        <label for="txt_fil_numdoc" align="left">Número de RUC/DNI:</label>
+        <input type="text" id="txt_fil_numdni" name="txt_fil_numdni" size="20" placeholder="" value="<?php echo $_POST['numdni']?>" />
 
-</center>
+        <br/>
+        <br/>
+        <a href="#" onClick="test()" id="btn_filtrardni">Buscar Documento</a>
+        <a href="#" onClick="venta_filtro()" id="btn_resfildni">Restablecer</a>
+    </fieldset>
 
-</fieldset>
+</div>
+
+
 </form>
