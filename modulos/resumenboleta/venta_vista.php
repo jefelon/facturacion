7188ebea4ecc43507e1b6b6123fdbfd73390af48
jheisonxxx <jheison.talaveragmail.com>
2018-07-02 13:17:31 -0500

    show alert disappear

--- modulos/resumenboleta/venta_vista.php
+++ modulos/resumenboleta/venta_vista.php
@@ -199,15 +199,12 @@ function cpe_ticket(id,tick)
             ticket:		tick
         }),
         beforeSend: function() {
-            $('#msj_venta_sunat2').html("Consultando...");
+            $('#msj_venta_sunat2').html("Consultando Ticket...");
             $('#msj_venta_sunat2').show(100);
-            showAlert('Consultando Ticket...');
         },
         success: function(data){
-            $('#msj_venta_sunat2').html(data.msj);
+            $('#msj_venta_sunat2').html(data.msj+': '+data.est);
             $('#msj_venta_sunat2').show();
-            showAlert(data.msj,data.est);
-
         },
         complete: function(){
             resumenboleta_tabla();
