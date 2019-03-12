dc9aee8598acadc604e8be193b83db5042533385
jefelon <ronilson_2006_2@hotmail.com>
2019-03-11 21:58:21 -0500

    por cobrar tabla...

--- modulos/venta/cVenta.php
+++ modulos/venta/cVenta.php
@@ -225,7 +225,34 @@ class cVenta{
 	$rst=$oCado->ejecute_sql($sql);
 	return $rst;
 	}
+    function mostrar_filtro_cobro_destino($fec1,$fec2,$doc_id,$cli_id,$est,$usu_id,$punven_id,$emp_id,$venmay){
+        $sql="SELECT ev.tb_encomiendaventa_id, vv.tb_viajeventa_id, v.tb_venta_id,v.tb_venta_est,v.tb_venta_tot,td.cs_tipodocumento_cod,v.tb_venta_ser,v.tb_venta_num,
+    v.tb_venta_fec, c.tb_cliente_nom,c.tb_cliente_doc ,v.cs_tipomoneda_id, d.tb_documento_ele, v.tb_venta_estsun, 
+    v.tb_venta_fecenvsun, td.cs_tipodocumento_cod, d.tb_documento_abr, v.tb_venta_numdoc, d.tb_documento_nom, 
+    v.tb_venta_valven, v.tb_venta_igv 
+	FROM tb_venta v
+	LEFT JOIN tb_cliente c ON v.tb_cliente_id=c.tb_cliente_id
+	LEFT JOIN cs_tipodocumento td ON v.cs_tipodocumento_id=td.cs_tipodocumento_id
+	INNER JOIN tb_documento d ON v.tb_documento_id=d.tb_documento_id
+	INNER JOIN tb_usuario u ON v.tb_usuario_id=u.tb_usuario_id
+	INNER JOIN tb_puntoventa pv ON v.tb_puntoventa_id=pv.tb_puntoventa_id
+	LEFT JOIN tb_encomiendaventa ev ON v.tb_venta_id=ev.tb_venta_id
+	LEFT JOIN tb_viajeventa vv ON vv.tb_venta_id=v.tb_venta_id	
+	WHERE  vv.tb_viajeventa_id IS NULL AND ev.tb_encomiendaventa_id IS NULL
+	AND tb_venta_fec BETWEEN '$fec1' AND '$fec2' ";
 
+        if($doc_id>0)$sql.=" AND v.tb_documento_id = $doc_id ";
+        if($cli_id>0)$sql.=" AND v.tb_cliente_id = $cli_id ";
+        if($usu_id>0)$sql.=" AND u.tb_usuario_id = $usu_id ";
+        if($punven_id>0)$sql.=" AND v.tb_puntoventa_id = $punven_id ";
+        if($venmay>0)$sql.=" AND v.tb_venta_may = $venmay ";
+        if($est!="")$sql.=" AND tb_venta_est LIKE '$est' ";
+
+        $sql.=" ORDER BY tb_venta_fec, tb_documento_nom, tb_venta_numdoc ";
+        $oCado = new Cado();
+        $rst=$oCado->ejecute_sql($sql);
+        return $rst;
+    }
 
     function mostrar_filtro_por_mes_anio($mes_id,$anio_id,$doc_id_1,$doc_id_2,$cli_id,$est,$usu_id,$punven_id,$emp_id,$venmay){
         $sql="SELECT * 
