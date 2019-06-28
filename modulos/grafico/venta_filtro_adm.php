c67f5e080ecd11ab9c7a55d1c65d04626f238815
jefelon <ronilson_2006_2@hotmail.com>
2019-06-27 20:05:53 -0500

    año 2019 grafico

--- modulos/grafico/venta_filtro_adm.php
+++ modulos/grafico/venta_filtro_adm.php
@@ -177,7 +177,8 @@ $(function() {
     </select>-->
     <label for="cmb_fil_ven_y">Año:</label>
 	<select name="cmb_fil_ven_y" id="cmb_fil_ven_y">
-        <option value="2018" selected="selected">2018</option>
+      <option value="2019" selected="selected">2019</option>
+      <option value="2018">2018</option>
 	  <option value="2017">2017</option>
 
     </select>
