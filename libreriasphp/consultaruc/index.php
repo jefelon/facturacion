33e78667a44ec81f51e5fb53adc9426ec47b2a3a
jefelon <ronilson_2006_2@hotmail.com>
2020-05-03 18:02:12 -0500

    fix search sunat

--- libreriasphp/consultaruc/index.php
+++ libreriasphp/consultaruc/index.php
@@ -3,13 +3,18 @@ require '../consultadni/simple_html_dom.php';
     $ruc = $_POST['vruc'];
     $consulta = file_get_html('http://ruc.aqpfact.pe/sunat/'.$ruc)->plaintext;
 
-    $datos = json_decode($consulta, true);
+    if(substr($ruc,0,2)==20){
+        echo $consulta;
+    }
+    else{
+        $datos = json_decode($consulta, true);
 
-    $data['Ruc'] = $datos{'result'}['Ruc'];
-    $data['RazonSocial'] = $datos{'result'}['RazonSocial'];
-    $data['Estado'] = $datos{'result'}['Estado'];
-    $data['Condicion']=$datos{'result'}['Condicion'];
-    $data['DireccionCompleta'] = $datos{'result'}['DireccionCompleta'];
+        $data['Ruc'] = $datos{'result'}['Ruc'];
+        $data['RazonSocial'] = $datos{'result'}['RazonSocial'];
+        $data['Estado'] = $datos{'result'}['Estado'];
+        $data['Condicion']=$datos{'result'}['Condicion'];
+        $data['DireccionCompleta'] = $datos{'result'}['DireccionCompleta'];
 
-    echo json_encode($data);
+        echo json_encode($data);
+    }
 ?>
