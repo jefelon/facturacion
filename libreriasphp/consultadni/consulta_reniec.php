ff4ce8f09ee46b846337ce57c36a6943a8aab63c
Efrain <efrainlp1995@gmail.com>
2021-01-23 18:42:17 -0500

    cambio server consulta ruc y dni

--- libreriasphp/consultadni/consulta_reniec.php
+++ libreriasphp/consultadni/consulta_reniec.php
@@ -5,18 +5,18 @@ require_once( __DIR__ . "/src/autoload.php" );
 
 //error_reporting(E_ALL ^ E_NOTICE);
 
-$essalud = new \EsSalud\EsSalud();
-$reniec = new \Reniec\Reniec();
+//$essalud = new \EsSalud\EsSalud();
+//$reniec = new \Reniec\Reniec();
 
 
 $dni = $_POST['dni'];
 
 $persona="Datos no encontrados, completa el nombre manualmente.";
-$search0 = $reniec->search( $dni );
-if( $search0->success == true){
-    $persona=$search0->result->apellidos." ".$search0->result->Nombres;
-}
-else{
+//$search0 = $reniec->search( $dni );
+//if( $search0->success == true){
+//    $persona=$search0->result->apellidos." ".$search0->result->Nombres;
+//}
+//else{
 
 
     $link="https://www.facturacionelectronica.us/facturacion/controller/ws_consulta_rucdni_v2.php?documento=DNI&usuario=10447915125&password=985511933&nro_documento=";
@@ -27,19 +27,19 @@ else{
         $consulta = @iconv($encoding, "utf-8//TRANSLIT//IGNORE", $consulta);
 
     $datos344=json_decode($consulta,true);
-    $dni=$datos344['result']['DNI'];
+    $dni=$datos344['result']['Dni'];
     $nom=$datos344['result']['Nombre'];
     $pat=$datos344['result']['Paterno'];
     $mat=$datos344['result']['Materno'];
 
-    $search1 = $essalud->search( $dni );
-    if($search1->success == true){
-        $persona=$search1->result->paterno." ".$search1->result->materno." ".$search1->result->nombre;
-    }
-    else if($consulta){
+//    $search1 = $essalud->search( $dni );
+//    if($search1->success == true){
+//        $persona=$search1->result->paterno." ".$search1->result->materno." ".$search1->result->nombre;
+//    }
+//    else if($consulta){
         $persona=$pat.' '.$mat.' '.$nom;
-    }
-}
+//    }
+//}
 
 $data['persona']=$persona;
 
--- libreriasphp/consultaruc/index.php
+++ libreriasphp/consultaruc/index.php
@@ -1,20 +1,22 @@
 <?php
 require '../consultadni/simple_html_dom.php';
     $ruc = $_POST['vruc'];
-    $consulta = file_get_html('http://ruc.aqpfact.pe/sunat/'.$ruc)->plaintext;
-
-    if(substr($ruc,0,2)==20){
-        echo $consulta;
+    $link="https://www.facturacionelectronica.us/facturacion/controller/ws_consulta_rucdni_v2.php?documento=RUC&usuario=10447915125&password=985511933&nro_documento=";
+    $consulta = file_get_contents($link.$ruc);
+    //Make sure we received utf-8:
+    $encoding = @mb_detect_encoding($consulta);
+    if ($encoding && strtoupper($encoding) != "utf-8") {
+        $consulta = @iconv($encoding, "utf-8//TRANSLIT//IGNORE", $consulta);
     }
-    else{
-        $datos = json_decode($consulta, true);
 
-        $data['Ruc'] = $datos{'result'}['Ruc'];
-        $data['RazonSocial'] = $datos{'result'}['RazonSocial'];
-        $data['Estado'] = $datos{'result'}['Estado'];
-        $data['Condicion']=$datos{'result'}['Condicion'];
-        $data['DireccionCompleta'] = $datos{'result'}['DireccionCompleta'];
+    $datos = json_decode($consulta, true);
 
-        echo json_encode($data);
-    }
+    $data['Ruc'] = $datos{'result'}['Ruc'];
+    $data['RazonSocial'] = $datos{'result'}['RazonSocial'];
+    $data['Estado'] = $datos{'result'}['Estado'];
+    $data['Condicion']=$datos{'result'}['Condicion'];
+    $data['DireccionCompleta'] = $datos{'result'}['Direccion'];
+
+    echo json_encode($data);
+
 ?>
